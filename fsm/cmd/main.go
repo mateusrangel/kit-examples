e5eb05e9ed5ba37d42d2a8c68800f682a41dde2e
Mateus Rangel <wolfeprmg@gmail.com>
2025-12-03 11:59:23 -0300

    refactor

--- fsm/cmd/main.go
+++ fsm/cmd/main.go
@@ -11,26 +11,27 @@ import (
 )
 
 func main() {
+	dispute := domain.NewDispute(uuid.New().String(), "RECEIVED")
+
 	repo, err := repository.New()
 	if err != nil {
 		panic(err)
 	}
 
-	dispute := domain.NewDispute(uuid.New().String(), "RECEIVED")
 	err = repo.CreateDispute(dispute)
 	if err != nil {
 		panic(err)
 	}
 
-	orderFSM := service.NewDisputeService(dispute, repo)
+	disputeService := service.NewDisputeService(dispute, repo)
 
-	fmt.Printf("BEFORE: %v\n", orderFSM.FSM.Current())
-	_ = orderFSM.FSM.ExecEvent("VALIDATION_SUCCEEDED")
-	fmt.Printf("AFTER: %v\n", orderFSM.FSM.Current())
+	fmt.Printf("BEFORE: %v\n", disputeService.FSM.Current())
+	_ = disputeService.FSM.ExecEvent("VALIDATION_SUCCEEDED")
+	fmt.Printf("AFTER: %v\n", disputeService.FSM.Current())
 
-	fmt.Printf("BEFORE: %v\n", orderFSM.FSM.Current())
-	_ = orderFSM.FSM.ExecEvent("CLAIM_CREATED")
-	fmt.Printf("AFTER: %v\n", orderFSM.FSM.Current())
+	fmt.Printf("BEFORE: %v\n", disputeService.FSM.Current())
+	_ = disputeService.FSM.ExecEvent("CLAIM_CREATED")
+	fmt.Printf("AFTER: %v\n", disputeService.FSM.Current())
 
-	fmt.Println(fsm.Visualize(orderFSM.FSM))
+	fmt.Println(fsm.Visualize(disputeService.FSM))
 }
